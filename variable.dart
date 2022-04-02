01f240a86263db2cd6fadb7b52e35095d3cb47dc
LACH Sokheng <lachsokheng.dev@gmail.com>
2022-04-02 11:37:27 +0700

    declare variable that check condition if else statement

--- variable.dart
+++ variable.dart
@@ -1,5 +1,20 @@
+import 'dart:io';
+
 void main() {
-  String name = 'sokheng';
-  int old = 18;
-  print('My name is ${name}and I am ${old}years old');
+  print('Please enter your score:');
+  int score = int.parse(stdin.readLineSync()!);
+
+  if (score >= 90) {
+    print('Your is score is Excellent :');
+  } else if (score >= 80 && score <= 89) {
+    print('Your score is Very good');
+  } else if (score >= 70 && score <= 70) {
+    print('Your score is good : ');
+  } else if (score > 60 && score <= 69) {
+    print('Your score is Fair :');
+  } else if (score >= 50 && score <= 59) {
+    print('Your score is Average:');
+  } else {
+    print('Your score is Failed: ');
+  }
 }
